6e69a47129bba11f8cf8e3bcfb2595fad6f2b6f1
rishabh416 <mittalr2008@gmail.com>
2024-08-16 12:50:26 +0400

    modifications to preprocessing parameters

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ iterations = 0
 
 # camera parameters in millimeters
 cameraFOV = 88
-focalLength = 0.57 # f
+focalLength = 5 # f
 camPixelSize = 0.001875 # d 3.6/1920
 camDistance = 60 # T
 
@@ -45,18 +45,9 @@ while True:
     # cv2.imshow("frame2blur", frame2blur)
 
     # process openCV image through depthAnything pipeline
-    image = Image.fromarray(cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB))
+    image = Image.fromarray(cv2.cvtColor(frame1blur, cv2.COLOR_BGR2RGB))
     result = pipe(image)
     depthimage = result["depth"]
-
-    f = plt.figure()
-    f.add_subplot(1,3, 1)
-    plt.imshow(frame1blur)
-    f.add_subplot(1,3, 2)
-    plt.imshow(frame2blur)
-    f.add_subplot(1,3, 3)
-    plt.imshow(np.array(depthimage))
-    plt.show(block=True)
 
     # find closest point from depth image array
     depthimagearray = np.array(depthimage)
@@ -65,7 +56,7 @@ while True:
     x, y = maxloc1
 
     # create 40x40 image template around closest point
-    halflength = 20
+    halflength = 10
     x_start = max(0, x - halflength)
     y_start = max(0, y - halflength)
     x_end = min(frame1.shape[1], x + halflength + 1)
@@ -73,15 +64,26 @@ while True:
     gridTemplate = frame1blur[y_start:y_end, x_start:x_end]
 
     # roi restriction
-    ymax = y + (2*halflength)+100
-    ymin = y - (2*halflength)-100
+    ymax = y + (2*halflength)
+    ymin = y - (2*halflength)
+    frame2ROI = frame2blur[ymin:ymax, :]
 
     # find matching template in image from camera 2
-    result = cv2.matchTemplate(frame2blur[ymin:ymax, :], gridTemplate, cv2.TM_SQDIFF) # TM_SQDIFF TM_CCOEFF
+    result = cv2.matchTemplate(frame2ROI, gridTemplate, cv2.TM_CCOEFF) # TM_SQDIFF TM_CCOEFF
     min_val2, max_val2, min_loc2, max_loc2 = cv2.minMaxLoc(result)
-    print("image2",max_val2, max_loc2)
     c, h, w = gridTemplate.shape[::-1]
     x_centerLoc = max_loc2[0] + w // 2
+    y_centerLoc = max_loc2[1] + h // 2
+    print("image2",max_val2, max_loc2)
+
+    f = plt.figure()
+    f.add_subplot(1,3, 1)
+    plt.imshow(frame1blur)
+    f.add_subplot(1,3, 2)
+    plt.imshow(frame2blur)
+    f.add_subplot(1,3, 3)
+    plt.imshow(np.array(frame2ROI))
+    plt.show(block=True)
 
     # calculate the distance of nearest object
     pixelDistance = (maxloc1[0]-x_centerLoc) # n1-n2
@@ -92,12 +94,13 @@ while True:
     depthAngle = (maxloc1[0]*cameraFOV)/imageWidth
 
     text = f'closest object at {objectDistance} centimeters, {5 * round(round(depthAngle) / 5)} degrees' 
+    print(text)
 
     # text to speech setup
-    tts = gTTS(text=text, lang='en')
-    tts.save("audio.mp3")
-    playsound("audio.mp3")
-    os.remove("audio.mp3") 
+    # tts = gTTS(text=text, lang='en')
+    # tts.save("audio.mp3")
+    # playsound("audio.mp3")
+    # os.remove("audio.mp3") 
 
     iterations += 1
 
